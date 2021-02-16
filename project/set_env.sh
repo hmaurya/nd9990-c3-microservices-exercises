38a3ff6177b19edd55822db41b7ea074b61a5767
hmaurya <gethimanshu@gmail.com>
2021-02-16 10:41:48 +0530

    made the project in working condition

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=udagramhmauryadev
+export POSTGRES_PASSWORD=p2udagram
+export POSTGRES_HOST=udagramhmauryadev.cjnng5rhulgd.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=udagramhmauryadev
+export AWS_BUCKET=udacity-website-hmaurya
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=helloworld
 export URL=http://localhost:8100
