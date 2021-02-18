d823dbfae3da7b876737310cd64e9a96419ae961
hmaurya <gethimanshu@gmail.com>
2021-02-18 06:27:42 +0530

    refactor

--- project/api-user/set_env.sh
+++ project/api-user/set_env.sh
@@ -1,11 +1,1 @@
-# This file is used for convenience of local development.
-# DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=udagramhmauryadev
-export POSTGRES_PASSWORD=p2udagram
-export POSTGRES_HOST=udagramhmauryadev.cjnng5rhulgd.us-east-2.rds.amazonaws.com
-export POSTGRES_DB=udagramhmauryadev
-export AWS_BUCKET=udacity-website-hmaurya
-export AWS_REGION=us-east-2
-export AWS_PROFILE=default
-export JWT_SECRET=helloworld
-export URL=http://localhost:8100
+
